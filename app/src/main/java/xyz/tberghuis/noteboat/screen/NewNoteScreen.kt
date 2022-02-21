82650887b2b0e1f5e4399ce7c4fe75aa8c77739b
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-02-21 12:24:01 +1100

    .

--- app/src/main/java/xyz/tberghuis/noteboat/screen/NewNoteScreen.kt
+++ app/src/main/java/xyz/tberghuis/noteboat/screen/NewNoteScreen.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
@@ -14,6 +16,7 @@ import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalContext
@@ -162,15 +165,26 @@ fun NewNoteContent(
   }
 
   Column {
-    TextField(
-      value = viewModel.noteTextFieldValueState.value,
-      // todo call ITextFieldViewModel.onValueChange
-      onValueChange = onValueChange,
-      modifier = Modifier
-        .focusRequester(focusRequester)
-        .navigationBarsWithImePadding()
-        .fillMaxSize()
-    )
+    Box {
+      TextField(
+        value = viewModel.noteTextFieldValueState.value,
+        // todo call ITextFieldViewModel.onValueChange
+        onValueChange = onValueChange,
+        modifier = Modifier
+          .focusRequester(focusRequester)
+          .navigationBarsWithImePadding()
+          .fillMaxSize()
+      )
+      // don't allow clicks on textfield
+      if (transcribing.value == TranscribingState.TRANSCRIBING) {
+        Box(
+          modifier = Modifier
+            .matchParentSize()
+            .alpha(0f)
+            .clickable(onClick = { }),
+        )
+      }
+    }
   }
 
   LaunchedEffect(Unit) {
