7c27ef65ec89e74e12394239060d4159aa094822
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-02-24 07:34:24 +1100

    .

--- app/src/main/java/xyz/tberghuis/noteboat/MainActivity.kt
+++ app/src/main/java/xyz/tberghuis/noteboat/MainActivity.kt
@@ -1,6 +1,7 @@
 package xyz.tberghuis.noteboat
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -45,6 +46,11 @@ class MainActivity : ComponentActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
+    val feature = intent.extras?.getString("feature")
+    Log.d("xxx", "feature $feature")
+
+
 
     CoroutineScope(Dispatchers.IO).launch {
       migrateLegacy()
