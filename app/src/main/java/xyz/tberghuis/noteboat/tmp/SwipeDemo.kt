c74c5a86f01dcda961b363173f2a080ba4320567
Tom Berghuis <thomas.berghuis@gmail.com>
2022-03-18 09:01:20 +1100

    .

--- app/src/main/java/xyz/tberghuis/noteboat/tmp/SwipeDemo.kt
+++ app/src/main/java/xyz/tberghuis/noteboat/tmp/SwipeDemo.kt
@@ -1,5 +1,11 @@
 package xyz.tberghuis.noteboat.tmp
 
+import android.annotation.SuppressLint
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -7,20 +13,39 @@ import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.insets.statusBarsPadding
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlin.math.roundToInt
+
+// todo like this
+//class NoteHolder(val note: String) {
+//  val isRevealed = false
+//}
+//
+//val noteList = mutableStateListOf<NoteHolder>(
+//  NoteHolder("item 1"),
+//  NoteHolder("item 2"),
+//  NoteHolder("item 3")
+//)
+
+
 
 
 val someStringArray = arrayOf("item 1", "item 2", "item 3")
 
+// i could use a set or a hashmap
 val revealedStringsList = MutableStateFlow(listOf<Int>())
 
+
+// on swipe card left (reveal actions)
 fun onItemExpanded(cardId: Int) {
   if (revealedStringsList.value.contains(cardId)) return
   revealedStringsList.value = revealedStringsList.value.toMutableList().also { list ->
@@ -28,6 +53,7 @@ fun onItemExpanded(cardId: Int) {
   }
 }
 
+// on swipe card right (hide actions)
 fun onItemCollapsed(cardId: Int) {
   if (!revealedStringsList.value.contains(cardId)) return
   revealedStringsList.value = revealedStringsList.value.toMutableList().also { list ->
@@ -39,6 +65,8 @@ fun onItemCollapsed(cardId: Int) {
 @Composable
 fun SwipeDemo() {
 
+  val revealedCardIds = revealedStringsList.collectAsState()
+
   Scaffold { _ ->
 
 
@@ -47,7 +75,9 @@ fun SwipeDemo() {
 
         Box {
           ActionsRow()
-          MyCard(i)
+          // should i add isRevealed to vm.noteHolder
+          // shouldn't really need it i think
+          MyCard(i, isRevealed = revealedCardIds.value.contains(i))
         }
 
 
@@ -59,13 +89,40 @@ fun SwipeDemo() {
 
 }
 
+
+@SuppressLint("UnusedTransitionTargetStateParameter")
 @Composable
-fun MyCard(i: Int) {
+fun MyCard(i: Int, isRevealed: Boolean) {
+
+  val transitionState = remember {
+    MutableTransitionState(isRevealed).apply {
+      targetState = !isRevealed
+    }
+  }
+  val transition = updateTransition(transitionState, "cardTransition")
+
+  val offsetTransition by transition.animateFloat(
+    label = "cardOffsetTransition",
+    transitionSpec = { tween(durationMillis = 500) },
+    targetValueByState = { if (isRevealed) 200f else 0f },
+
+    )
+
+
   Card(
     Modifier
       .fillMaxWidth()
       .padding(10.dp)
-      .height(50.dp),
+      .height(50.dp)
+      .offset { -IntOffset(offsetTransition.roundToInt(), 0) }
+      .pointerInput(Unit) {
+        detectHorizontalDragGestures { _, dragAmount ->
+          when {
+            dragAmount < -10 -> onItemExpanded(i)
+            dragAmount > 10 -> onItemCollapsed(i)
+          }
+        }
+      },
 
     ) {
     Box(contentAlignment = Alignment.Center) {
