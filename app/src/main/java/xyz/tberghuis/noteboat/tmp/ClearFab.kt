51259a0de790e0667ffcb2233dc0f3fd661c29b7
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-05-07 16:29:48 +1000

    .

---
+++ app/src/main/java/xyz/tberghuis/noteboat/tmp/ClearFab.kt
@@ -0,0 +1,16 @@
+package xyz.tberghuis.noteboat.tmp
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ClearFab() {
+  Text("hello clear FAB")
+}
+
+@Preview(showSystemUi=true)
+@Composable
+fun PreviewClearFab() {
+  ClearFab()
+}
