060a30874a61d91d5b56d4fd67295b1c37f517f8
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-08-07 11:56:51 +1000

    .

--- app/src/main/java/xyz/tberghuis/noteboat/screen/SettingsScreen.kt
+++ app/src/main/java/xyz/tberghuis/noteboat/screen/SettingsScreen.kt
@@ -1,28 +1,23 @@
 package xyz.tberghuis.noteboat.screen
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 
 @Preview
 @Composable
 fun SettingsScreen() {
 
   Scaffold(
-    modifier = Modifier.systemBarsPadding(), // why is this not needed for home route???
+    modifier = Modifier.systemBarsPadding(), // why is this not needed for other routes???
+
     topBar = {
       TopAppBar(
         title = { Text("from R.strings") },
