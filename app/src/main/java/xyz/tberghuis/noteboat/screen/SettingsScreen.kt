49ca4381f2602191d64ad4412730bd53ef4c0f64
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-08-07 11:19:52 +1000

    .

--- app/src/main/java/xyz/tberghuis/noteboat/screen/SettingsScreen.kt
+++ app/src/main/java/xyz/tberghuis/noteboat/screen/SettingsScreen.kt
@@ -1,9 +1,36 @@
 package xyz.tberghuis.noteboat.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 
+@Preview
 @Composable
 fun SettingsScreen() {
-  Text("settings screen")
+
+  Scaffold() { padding ->
+    Column(
+      modifier = Modifier
+        .padding(padding)
+        .fillMaxSize()
+        .background(Color.Green)
+    ) {
+      Text(
+        "settings screen",
+            modifier= Modifier.fillMaxSize(),
+        color = Color.Black,
+      )
+
+    }
+  }
+
 }
+
+
