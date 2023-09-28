a8838d445ed1182b450b246f6b5f828cd5524087
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-09-28 14:58:49 +1000

    .

--- app/src/main/java/xyz/tberghuis/noteboat/tmp/TmpScreen.kt
+++ app/src/main/java/xyz/tberghuis/noteboat/tmp/TmpScreen.kt
@@ -1,9 +1,26 @@
 package xyz.tberghuis.noteboat.tmp
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun TmpScreen() {
-  Text("hello tmp")
+
+  Scaffold(
+    bottomBar = {
+      Text("bottom bar")
+    },
+  ) { paddingValues ->
+    Column(Modifier.padding(paddingValues)) {
+      TextField(
+        value = "text field",
+        onValueChange = {},
+      )
+    }
+  }
 }
