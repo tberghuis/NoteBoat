4208acb5660d66f5befb8ab09b06f25a7e24e22a
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-09-28 15:45:18 +1000

    .

--- app/src/main/java/xyz/tberghuis/noteboat/tmp/TmpScreen.kt
+++ app/src/main/java/xyz/tberghuis/noteboat/tmp/TmpScreen.kt
@@ -1,33 +1,39 @@
 package xyz.tberghuis.noteboat.tmp
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.BottomAppBar
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.MicOff
+import androidx.compose.material.icons.filled.Save
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun TmpScreen() {
-
   Scaffold(
     bottomBar = {
       BottomAppBar(
-
+        backgroundColor = MaterialTheme.colors.background,
+        contentPadding = PaddingValues(10.dp),
       ) {
         Spacer(Modifier.weight(1f))
-        IconButton(onClick = { /* doSomething() */ }) {
-          Icon(Icons.Filled.Favorite, contentDescription = "Localized description")
+        FloatingActionButton(
+          modifier = Modifier.padding(end = 10.dp),
+          onClick = { /* doSomething() */ }) {
+          Icon(Icons.Filled.MicOff, contentDescription = "Localized description")
         }
-        IconButton(onClick = { /* doSomething() */ }) {
-          Icon(Icons.Filled.Favorite, contentDescription = "Localized description")
+        FloatingActionButton(
+          onClick = { /* doSomething() */ }) {
+          Icon(Icons.Filled.Save, contentDescription = "Localized description")
         }
       }
     },
