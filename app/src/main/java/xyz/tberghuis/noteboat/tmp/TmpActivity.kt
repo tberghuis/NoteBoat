905002a49b73c43feac430d54042fba5385bcd34
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-09-28 14:52:36 +1000

    .

---
+++ app/src/main/java/xyz/tberghuis/noteboat/tmp/TmpActivity.kt
@@ -0,0 +1,23 @@
+package xyz.tberghuis.noteboat.tmp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import xyz.tberghuis.noteboat.ui.theme.NoteBoatTheme
+
+class TmpActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      NoteBoatTheme {
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+          TmpScreen()
+        }
+      }
+    }
+  }
+}
---
+++ app/src/main/java/xyz/tberghuis/noteboat/tmp/TmpScreen.kt
@@ -0,0 +1,9 @@
+package xyz.tberghuis.noteboat.tmp
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TmpScreen() {
+  Text("hello tmp")
+}
