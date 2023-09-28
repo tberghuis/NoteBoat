444b76e53ee0f0e2fd6751d4da247e874bf2ce50
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-09-28 15:11:00 +1000

    .

--- app/src/main/java/xyz/tberghuis/noteboat/tmp/TmpScreen.kt
+++ app/src/main/java/xyz/tberghuis/noteboat/tmp/TmpScreen.kt
@@ -1,10 +1,16 @@
 package xyz.tberghuis.noteboat.tmp
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
@@ -13,7 +19,17 @@ fun TmpScreen() {
 
   Scaffold(
     bottomBar = {
-      Text("bottom bar")
+      BottomAppBar(
+
+      ) {
+        Spacer(Modifier.weight(1f))
+        IconButton(onClick = { /* doSomething() */ }) {
+          Icon(Icons.Filled.Favorite, contentDescription = "Localized description")
+        }
+        IconButton(onClick = { /* doSomething() */ }) {
+          Icon(Icons.Filled.Favorite, contentDescription = "Localized description")
+        }
+      }
     },
   ) { paddingValues ->
     Column(Modifier.padding(paddingValues)) {
