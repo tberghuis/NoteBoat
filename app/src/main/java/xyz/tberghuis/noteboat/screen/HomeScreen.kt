0005e5a3fd341a3a2dc69466d0c9b82d0ae2325f
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-01-11 12:10:51 +1100

    .

--- app/src/main/java/xyz/tberghuis/noteboat/screen/HomeScreen.kt
+++ app/src/main/java/xyz/tberghuis/noteboat/screen/HomeScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -35,7 +34,6 @@ import xyz.tberghuis.noteboat.R
 
 @Composable
 fun HomeScreen(
-//  viewModel: HomeViewModel = hiltViewModel(),
   navController: NavHostController
 ) {
   val scaffoldState = rememberScaffoldState()
