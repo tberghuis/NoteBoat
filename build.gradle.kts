cf62cc84067b8e711de9946e3bb3689cb5ea9942
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-07-05 14:04:34 +1000

    .

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.jetbrains.kotlin.android) apply false
+  alias(libs.plugins.ksp) apply false
+//  id("com.android.library" version '8.5.0' apply false)
+}
