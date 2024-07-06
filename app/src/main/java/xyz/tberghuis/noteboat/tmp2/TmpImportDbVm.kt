bed9f4c40db66219dafe3c05058751c4d44f5c12
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-07-06 13:15:10 +1000

    .

--- app/src/main/java/xyz/tberghuis/noteboat/tmp2/TmpImportDbVm.kt
+++ app/src/main/java/xyz/tberghuis/noteboat/tmp2/TmpImportDbVm.kt
@@ -4,8 +4,6 @@ import android.app.Application
 import android.net.Uri
 import androidx.lifecycle.AndroidViewModel
 import java.io.File
-import java.io.FileOutputStream
-import java.io.InputStream
 
 class TmpImportDbVm(
   val app: Application,
@@ -15,24 +13,28 @@ class TmpImportDbVm(
   fun copyInputStream() {
     val newFile = File(app.filesDir, "newfile.db")
     val inputStream = app.contentResolver.openInputStream(importFileUri!!)
-    copyStreamToFile(inputStream!!, newFile)
+    // https://www.baeldung.com/kotlin/inputstream-to-file
+    inputStream!!.use { input ->
+      newFile.outputStream().use { output ->
+        input.copyTo(output)
+      }
+    }
   }
 }
 
 // https://stackoverflow.com/questions/10854211/android-store-inputstream-in-file
 // will this overwrite a file correctly?
-fun copyStreamToFile(inputStream: InputStream, outputFile: File) {
-  inputStream.use { input ->
-    val outputStream = FileOutputStream(outputFile)
-    outputStream.use { output ->
-      val buffer = ByteArray(4 * 1024) // buffer size
-      while (true) {
-        val byteCount = input.read(buffer)
-        if (byteCount < 0) break
-        output.write(buffer, 0, byteCount)
-      }
-      output.flush()
-    }
-  }
-}
-
+//fun copyStreamToFile(inputStream: InputStream, outputFile: File) {
+//  inputStream.use { input ->
+//    val outputStream = FileOutputStream(outputFile)
+//    outputStream.use { output ->
+//      val buffer = ByteArray(4 * 1024) // buffer size
+//      while (true) {
+//        val byteCount = input.read(buffer)
+//        if (byteCount < 0) break
+//        output.write(buffer, 0, byteCount)
+//      }
+//      output.flush()
+//    }
+//  }
+//}
