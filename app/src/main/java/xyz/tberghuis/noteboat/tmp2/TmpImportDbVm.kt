eb36f08fcfc62b1779bba9b8b0ed2a8b49297952
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-07-09 13:56:25 +1000

    .

--- app/src/main/java/xyz/tberghuis/noteboat/tmp2/TmpImportDbVm.kt
+++ app/src/main/java/xyz/tberghuis/noteboat/tmp2/TmpImportDbVm.kt
@@ -2,7 +2,6 @@ package xyz.tberghuis.noteboat.tmp2
 
 import android.app.Application
 import android.net.Uri
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.room.Room
@@ -10,41 +9,14 @@ import java.io.File
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
-import xyz.tberghuis.noteboat.DB_FILENAME
 import xyz.tberghuis.noteboat.MainApplication
 import xyz.tberghuis.noteboat.data.AppDatabase
-import xyz.tberghuis.noteboat.data.Note
 import xyz.tberghuis.noteboat.utils.logd
 
 class TmpImportDbVm(
   application: Application,
 ) : AndroidViewModel(application) {
   private val mainApp = (application as MainApplication)
-
-//  fun openAndCloseDb() {
-//    viewModelScope.launch(IO) {
-//      logd("stop db")
-//      logd("isopen ${mainApp.appDatabase.isOpen}")
-//      if (!mainApp.appDatabase.isOpen) {
-//        mainApp.appDatabase.openHelper.readableDatabase
-//        logd("isopen ${mainApp.appDatabase.isOpen}")
-//      }
-//      // deletes .db-shm and .db-wal files
-//      mainApp.appDatabase.close()
-//      logd("isopen ${mainApp.appDatabase.isOpen}")
-//    }
-//  }
-
-//  fun importSelectedDb() {
-//    val dbFile = mainApp.getDatabasePath(DB_FILENAME)
-//    val inputStream = mainApp.contentResolver.openInputStream(importFileUri!!)
-//    // https://www.baeldung.com/kotlin/inputstream-to-file
-//    inputStream!!.use { input ->
-//      dbFile.outputStream().use { output ->
-//        input.copyTo(output)
-//      }
-//    }
-//  }
 
   fun importDb(importDbUri: Uri) {
     viewModelScope.launch(IO) {
@@ -82,20 +54,3 @@ class TmpImportDbVm(
     }
   }
 }
-
-// https://stackoverflow.com/questions/10854211/android-store-inputstream-in-file
-// will this overwrite a file correctly?
-//fun copyStreamToFile(inputStream: InputStream, outputFile: File) {
-//  inputStream.use { input ->
-//    val outputStream = FileOutputStream(outputFile)
-//    outputStream.use { output ->
-//      val buffer = ByteArray(4 * 1024) // buffer size
-//      while (true) {
-//        val byteCount = input.read(buffer)
-//        if (byteCount < 0) break
-//        output.write(buffer, 0, byteCount)
-//      }
-//      output.flush()
-//    }
-//  }
-//}
