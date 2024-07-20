43a2b6e07a83872a7938f5d0e72771bae5082214
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-07-21 09:43:00 +1000

    .

--- app/src/main/java/xyz/tberghuis/noteboat/tmp/TmpScreen.kt
+++ app/src/main/java/xyz/tberghuis/noteboat/tmp/TmpScreen.kt
@@ -4,6 +4,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import xyz.tberghuis.noteboat.ui.theme.NoteBoatTheme
 
 @Composable
 fun TmpScreen(
@@ -11,5 +17,15 @@ fun TmpScreen(
 ) {
   Column {
     Text("hello tmp screen")
+  }
+}
+
+@Preview
+@Composable
+fun TmpScreenPreview() {
+  NoteBoatTheme {
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+      TmpScreen()
+    }
   }
 }
