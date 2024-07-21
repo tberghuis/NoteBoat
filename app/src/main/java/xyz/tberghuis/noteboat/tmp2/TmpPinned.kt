e5596625787aeb22ffb0d596de8f1d65d0a01a0b
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-07-21 10:20:03 +1000

    .

--- app/src/main/java/xyz/tberghuis/noteboat/tmp2/TmpPinned.kt
+++ app/src/main/java/xyz/tberghuis/noteboat/tmp2/TmpPinned.kt
@@ -2,9 +2,18 @@ package xyz.tberghuis.noteboat.tmp2
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Pin
+import androidx.compose.material.icons.filled.PushPin
+import androidx.compose.material.icons.outlined.PushPin
+import androidx.compose.material.icons.rounded.PushPin
+import androidx.compose.material.icons.sharp.PushPin
+import androidx.compose.material.icons.twotone.PushPin
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -27,6 +36,16 @@ fun TmpNoteCard() {
     Column(
       modifier = Modifier.padding(10.dp)
     ) {
+
+//      Icon(Icons.Filled.PushPin, "complete")
+//      Icon(Icons.Sharp.PushPin, "complete")
+//      Icon(Icons.Outlined.PushPin, "complete")
+      Icon(
+        Icons.TwoTone.PushPin, "pinned",
+        modifier = Modifier.offset(x = (-8).dp, y = (-5).dp),
+      )
+//      Icon(Icons.Rounded.PushPin, "complete")
+
       Text("hello note")
     }
   }
