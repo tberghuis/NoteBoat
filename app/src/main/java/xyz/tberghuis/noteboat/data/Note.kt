a1d8a09b670fe147b4e365855e5b865305407491
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-12-06 09:17:14 +1100

    .

--- app/src/main/java/xyz/tberghuis/noteboat/data/Note.kt
+++ app/src/main/java/xyz/tberghuis/noteboat/data/Note.kt
@@ -1,6 +1,13 @@
 package xyz.tberghuis.noteboat.data
 
-import androidx.room.*
+import androidx.room.ColumnInfo
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Entity(
