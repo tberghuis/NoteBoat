5293877e87b16aa04a49c3ba28f0370adab8fa8a
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-12-07 09:05:52 +1100

    .

--- app/src/main/java/xyz/tberghuis/noteboat/tmp2/NotesList.kt
+++ app/src/main/java/xyz/tberghuis/noteboat/tmp2/NotesList.kt
@@ -1,24 +1,10 @@
 package xyz.tberghuis.noteboat.tmp2
 
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.OutlinedCard
-import androidx.compose.material3.SwipeToDismissBox
-import androidx.compose.material3.SwipeToDismissBoxValue
-import androidx.compose.material3.Text
-import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
@@ -32,41 +18,7 @@ fun NotesList() {
     items(trashNotes.value,
       key = { it.hashCode() }
     ) { note ->
-
-
-      val dismissState = rememberSwipeToDismissBoxState()
-      SwipeToDismissBox(
-        state = dismissState,
-        backgroundContent = {
-          val color by
-          animateColorAsState(
-            when (dismissState.targetValue) {
-              SwipeToDismissBoxValue.Settled -> Color.LightGray
-              SwipeToDismissBoxValue.StartToEnd -> Color.Green
-              SwipeToDismissBoxValue.EndToStart -> Color.Red
-            }
-          )
-          Box(
-            Modifier
-              .fillMaxSize()
-              .background(color)
-          )
-        }
-      ) {
-
-        TmpNoteCard(note)
-
-//        OutlinedCard(shape = RectangleShape) {
-//          ListItem(
-//            headlineContent = { Text("Cupcake") },
-//            supportingContent = { Text("Swipe me left or right!") }
-//          )
-//        }
-
-
-      }
-
-
+      TmpNoteCard(note)
     }
   }
 }
--- app/src/main/java/xyz/tberghuis/noteboat/tmp2/TmpNoteCard.kt
+++ app/src/main/java/xyz/tberghuis/noteboat/tmp2/TmpNoteCard.kt
@@ -5,32 +5,63 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import xyz.tberghuis.noteboat.LocalNavController
 import xyz.tberghuis.noteboat.data.Note
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxValue
+import androidx.compose.material3.rememberSwipeToDismissBoxState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun TmpNoteCard(
   note: Note,
 ) {
-  val navController = LocalNavController.current
-  Card(
-    modifier = Modifier
-      .fillMaxWidth()
-      // in future use Spacer instead of unfunctional padding
-      .padding(vertical = 5.dp)
-      .clickable {
-        // todo long press "restore note" dialog
-      },
-    elevation = 10.dp
-  ) {
-    Column(
-      modifier = Modifier.padding(10.dp)
-    ) {
-      Text(note.noteText)
+
+
+  val dismissState = rememberSwipeToDismissBoxState()
+  SwipeToDismissBox(
+    state = dismissState,
+    backgroundContent = {
+      val color by
+      animateColorAsState(
+        when (dismissState.targetValue) {
+          SwipeToDismissBoxValue.Settled -> Color.LightGray
+          SwipeToDismissBoxValue.StartToEnd -> Color.Green
+          SwipeToDismissBoxValue.EndToStart -> Color.Red
+        }, label = "ColorAnimation"
+      )
+      Box(
+        Modifier
+          .fillMaxSize()
+          .background(color)
+      )
     }
+  ) {
+    Card(
+      modifier = Modifier
+        .fillMaxWidth()
+        .padding(vertical = 5.dp)
+        .clickable {
+          // todo long press "restore note" dialog
+        },
+      elevation = 10.dp
+    ) {
+      Column(
+        modifier = Modifier.padding(10.dp)
+      ) {
+        Text(note.noteText)
+      }
+    }
+
   }
+
+
 }
