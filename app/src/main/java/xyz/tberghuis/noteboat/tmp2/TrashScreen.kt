7fdbcfc91c9ac76494b76f10e05455279b7a059b
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-12-07 07:09:39 +1100

    .

--- app/src/main/java/xyz/tberghuis/noteboat/tmp2/TrashScreen.kt
+++ app/src/main/java/xyz/tberghuis/noteboat/tmp2/TrashScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import xyz.tberghuis.noteboat.screen.HomeContent
 
 @Composable
 fun TrashScreen() {
--- app/src/main/java/xyz/tberghuis/noteboat/tmp2/TrashScreenVm.kt
+++ app/src/main/java/xyz/tberghuis/noteboat/tmp2/TrashScreenVm.kt
@@ -3,10 +3,17 @@ package xyz.tberghuis.noteboat.tmp2
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.SavedStateHandle
+import xyz.tberghuis.noteboat.MainApplication
 
 class TrashScreenVm(
   application: Application,
   savedStateHandle: SavedStateHandle
 ) :
   AndroidViewModel(application) {
+
+  val noteDao = (application as MainApplication).appDatabase.noteDao()
+  // todo
+  val trashNotes = noteDao.getAll()
+
+
 }
