2fbc469c29876da2b43b506f34c762ce8bca0368
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-12-07 08:42:40 +1100

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,7 @@ dependencies {
   implementation(libs.androidx.ui.graphics)
   implementation(libs.androidx.ui.tooling.preview)
   implementation(libs.androidx.material)
+  implementation(libs.androidx.material3.android)
 
   testImplementation(libs.junit)
   androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/xyz/tberghuis/noteboat/tmp2/NotesList.kt
+++ app/src/main/java/xyz/tberghuis/noteboat/tmp2/NotesList.kt
@@ -1,15 +1,24 @@
 package xyz.tberghuis.noteboat.tmp2
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Text
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxValue
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
@@ -23,19 +32,41 @@ fun NotesList() {
     items(trashNotes.value,
       key = { it.hashCode() }
     ) { note ->
-      Box(
-        Modifier.fillMaxWidth(),
-        contentAlignment = Alignment.Center
+
+
+      val dismissState = rememberSwipeToDismissBoxState()
+      SwipeToDismissBox(
+        state = dismissState,
+        backgroundContent = {
+          val color by
+          animateColorAsState(
+            when (dismissState.targetValue) {
+              SwipeToDismissBoxValue.Settled -> Color.LightGray
+              SwipeToDismissBoxValue.StartToEnd -> Color.Green
+              SwipeToDismissBoxValue.EndToStart -> Color.Red
+            }
+          )
+          Box(
+            Modifier
+              .fillMaxSize()
+              .background(color)
+          )
+        }
       ) {
-        TmpNoteCard(note)
-//        ActionsCard(note)
-//        NoteCard(
-//          note,
-//          offsetNotes.value.contains(note),
-//          viewModel::onRevealActions,
-//          viewModel::onHideActions
-//        )
+
+//        TmpNoteCard(note)
+
+        OutlinedCard(shape = RectangleShape) {
+          ListItem(
+            headlineContent = { Text("Cupcake") },
+            supportingContent = { Text("Swipe me left or right!") }
+          )
+        }
+
+
       }
+
+
     }
   }
 }
