7364e370b2ffc8aff5a2a811e1af56ce90bfec23
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-12-07 06:55:10 +1100

    .

---
+++ app/src/main/java/xyz/tberghuis/noteboat/tmp2/NotesList.kt
@@ -0,0 +1,8 @@
+package xyz.tberghuis.noteboat.tmp2
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun NotesList() {
+
+}
---
+++ app/src/main/java/xyz/tberghuis/noteboat/tmp2/TrashScreenVm.kt
@@ -0,0 +1,12 @@
+package xyz.tberghuis.noteboat.tmp2
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.SavedStateHandle
+
+class TrashScreenVm(
+  application: Application,
+  savedStateHandle: SavedStateHandle
+) :
+  AndroidViewModel(application) {
+}
