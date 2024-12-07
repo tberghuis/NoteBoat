51a8f58f6bb1924f45f147e3dcb335fb8ff2c05c
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-12-07 11:18:41 +1100

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     applicationId = "site.thomasberghuis.noteboat"
     minSdk = 27
     targetSdk = 34
-    versionCode = 21
-    versionName = "2.10.0"
+    versionCode = 22
+    versionName = "2.11.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables {
