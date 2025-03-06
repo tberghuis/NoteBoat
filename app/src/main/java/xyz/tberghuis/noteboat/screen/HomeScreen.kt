216e4ba1a8f0c3a87ab0ee2af6f8612045e31472
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-03-06 11:38:37 +1100

    .

--- app/src/main/java/xyz/tberghuis/noteboat/screen/HomeScreen.kt
+++ app/src/main/java/xyz/tberghuis/noteboat/screen/HomeScreen.kt
@@ -7,7 +7,16 @@ import androidx.compose.animation.core.rememberTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
