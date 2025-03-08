cf45fb9ae452b7fdd04e69ce8c392eaf2b687cd5
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-03-08 14:45:32 +1100

    .

--- app/src/main/java/xyz/tberghuis/noteboat/tmp/tmp01/tmprepo.kt
+++ app/src/main/java/xyz/tberghuis/noteboat/tmp/tmp01/tmprepo.kt
@@ -1,7 +1,13 @@
 package xyz.tberghuis.noteboat.tmp.tmp01
 
+import androidx.room.Dao
 import androidx.room.Entity
+import androidx.room.Insert
 import androidx.room.PrimaryKey
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+import androidx.room.Delete
+import androidx.room.Update
 
 @Entity
 data class TmpCategory(
@@ -16,3 +22,18 @@ data class TmpNote(
   val noteText: String,
   val categoryId: Int,
 )
+
+@Dao
+interface TmpCategoryDao {
+  @Query("SELECT * FROM TmpCategory")
+  fun getAll(): Flow<List<TmpCategory>>
+
+  @Insert
+  suspend fun insertAll(vararg category: TmpCategory)
+
+  @Delete
+  suspend fun delete(vararg category: TmpCategory)
+
+  @Update
+  suspend fun update(category: TmpCategory)
+}
