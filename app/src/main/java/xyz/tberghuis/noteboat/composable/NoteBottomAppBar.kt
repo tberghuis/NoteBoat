b822f146725c6038bbec59eb77fc984b93e33cad
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-03-25 15:03:12 +1100

    .

--- app/src/main/java/xyz/tberghuis/noteboat/composable/NoteBottomAppBar.kt
+++ app/src/main/java/xyz/tberghuis/noteboat/composable/NoteBottomAppBar.kt
@@ -11,19 +11,15 @@ import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Mic
-import androidx.compose.material.icons.filled.MicOff
 import androidx.compose.material.icons.filled.Save
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusProperties
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -37,55 +33,13 @@ fun NoteBottomAppBar(
   transcribingStateFlow: MutableStateFlow<TranscribingState>,
   onComplete: () -> Unit,
 ) {
-  val keyboardController = LocalSoftwareKeyboardController.current
-  val transcribingState = transcribingStateFlow.collectAsState()
-  val context = LocalContext.current
-
-  val launcher = rememberLauncherForActivityResult(
-    ActivityResultContracts.RequestPermission()
-  ) {}
-
-//  var fabOnClick: () -> Unit = {
-//    when (PackageManager.PERMISSION_GRANTED) {
-//      ContextCompat.checkSelfPermission(
-//        context,
-//        Manifest.permission.RECORD_AUDIO
-//      ) -> {
-//        keyboardController?.hide()
-//        transcribingStateFlow.value = TranscribingState.TRANSCRIBING
-//      }
-//
-//      else -> {
-//        launcher.launch(Manifest.permission.RECORD_AUDIO)
-//      }
-//    }
-//  }
-
-//  var fabIcon = @Composable { Icon(Icons.Filled.Mic, "speech input") }
-
-//  if (transcribingState.value == TranscribingState.TRANSCRIBING) {
-//    fabOnClick = {
-//      transcribingStateFlow.value = TranscribingState.NOT_TRANSCRIBING
-//    }
-//    fabIcon = @Composable { Icon(Icons.Filled.MicOff, "stop speech input") }
-//  }
-
   BottomAppBar(
     modifier = Modifier
       .imePadding(),
     contentPadding = PaddingValues(20.dp),
   ) {
     Spacer(Modifier.weight(1f))
-
-//    FloatingActionButton(
-//      modifier = Modifier.padding(end = 20.dp),
-//      onClick = fabOnClick
-//    ) {
-//      fabIcon()
-//    }
-
     PushToTranscribe(transcribingStateFlow)
-
     FloatingActionButton(
       onClick = onComplete
     ) {
