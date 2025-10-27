57a9eb536594bb464f3bc63bd6aca786e0e36fb6
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-10-27 15:39:43 +1100

    .

--- app/src/main/java/xyz/tberghuis/noteboat/composable/HomeContent.kt
+++ app/src/main/java/xyz/tberghuis/noteboat/composable/HomeContent.kt
@@ -20,7 +20,6 @@ import xyz.tberghuis.noteboat.data.Note
 fun HomeContent() {
   val viewModel: HomeViewModel = viewModel()
   val allNotes = viewModel.allNotes.collectAsState()
-//  val offsetNotes = viewModel.offsetNotes.collectAsState(setOf())
   val offsetNotes = viewModel.offsetNotes.collectAsState()
   LazyColumn(
     contentPadding = PaddingValues(10.dp)
