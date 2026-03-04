6795499ddd86b41ccdceb0d9b5f7a7b5d133c0e9
Thomas Berghuis <thomas.berghuis@gmail.com>
2026-03-04 14:41:01 +0800

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   alias(libs.plugins.android.application) apply false
-//  alias(libs.plugins.jetbrains.kotlin.android) apply false
   alias(libs.plugins.ksp) apply false
 //  id("com.android.library" version '8.5.0' apply false)
   alias(libs.plugins.compose.compiler) apply false
