57a6277f18bf74f13a6b0205807cd7abecf72342
Thomas Berghuis <thomas.berghuis@gmail.com>
2026-03-22 11:19:06 +0800

    .

--- composeApp/src/commonMain/kotlin/xyz/tberghuis/noteboat/tmp/tmp02/TmpScreen.kt
+++ composeApp/src/commonMain/kotlin/xyz/tberghuis/noteboat/tmp/tmp02/TmpScreen.kt
@@ -4,11 +4,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import xyz.tberghuis.noteboat.ui.theme.NoteBoatTheme
 
 @Composable
 fun TmpScreen(
@@ -27,7 +27,7 @@ fun TmpScreen(
 @Preview
 @Composable
 fun TmpScreenPreview() {
-  NoteBoatTheme {
+  MaterialTheme {
     Surface(
       modifier = Modifier.fillMaxSize(),
     ) {
