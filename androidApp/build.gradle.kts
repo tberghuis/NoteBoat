f8961464d500b497386142abcf71c2115582da16
Thomas Berghuis <thomas.berghuis@gmail.com>
2026-04-02 16:20:20 +0800

    .

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -68,7 +68,7 @@ dependencies {
 
 //  implementation(platform(libs.androidx.compose.bom))
 
-  implementation(libs.androidx.material.icons.extended)
+//  implementation(libs.androidx.material.icons.extended)
 
   implementation(libs.androidx.navigation.compose)
   implementation(libs.androidx.datastore.preferences)
