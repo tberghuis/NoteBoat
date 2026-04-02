6301313eb17b1d665b3a456558fd762e02a8bf62
Thomas Berghuis <thomas.berghuis@gmail.com>
2026-04-02 10:25:29 +0800

    .

--- composeApp/src/jvmMain/kotlin/xyz/tberghuis/noteboat/di/RoomFactory.jvm.kt
+++ composeApp/src/jvmMain/kotlin/xyz/tberghuis/noteboat/di/RoomFactory.jvm.kt
@@ -1,9 +1,14 @@
 package xyz.tberghuis.noteboat.di
 
+import xyz.tberghuis.noteboat.data.AppDatabase
 import xyz.tberghuis.noteboat.database.TmpAppDatabase
 
 actual class RoomFactory {
   actual fun createRoomDatabase(): TmpAppDatabase {
+    TODO("Not yet implemented")
+  }
+
+  actual fun createAppDatabase(): AppDatabase {
     TODO("Not yet implemented")
   }
 }
