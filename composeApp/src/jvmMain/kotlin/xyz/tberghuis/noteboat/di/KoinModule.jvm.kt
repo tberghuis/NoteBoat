758d01613b93597c7f46b5fc3cd49427774cb1a1
Thomas Berghuis <thomas.berghuis@gmail.com>
2026-04-06 14:18:11 +0800

    .

--- composeApp/src/jvmMain/kotlin/xyz/tberghuis/noteboat/di/KoinModule.jvm.kt
+++ composeApp/src/jvmMain/kotlin/xyz/tberghuis/noteboat/di/KoinModule.jvm.kt
@@ -2,10 +2,11 @@ package xyz.tberghuis.noteboat.di
 
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
+import xyz.tberghuis.noteboat.data.AppDatabaseFactory
 import xyz.tberghuis.noteboat.tmp.tmp03.TmpSpeechControllerFactory
 
 actual val platformModule = module {
 
   singleOf(::TmpSpeechControllerFactory)
-
+  single<AppDatabaseFactory> { AppDatabaseFactory() }
 }
