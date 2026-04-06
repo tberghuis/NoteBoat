0c9224cd1bd6d6821b667145f38b4b02c044c49d
Thomas Berghuis <thomas.berghuis@gmail.com>
2026-04-06 15:00:45 +0800

    .

--- composeApp/src/androidMain/kotlin/xyz/tberghuis/noteboat/data/AppDatabaseFactory.android.kt
+++ composeApp/src/androidMain/kotlin/xyz/tberghuis/noteboat/data/AppDatabaseFactory.android.kt
@@ -10,13 +10,24 @@ actual class AppDatabaseFactory(
   private val app: Application,
 ) {
   actual fun create(): AppDatabase {
-    val dbFile = app.getDatabasePath(DB_FILENAME)
-    return Room
-      .databaseBuilder<AppDatabase>(
-        context = app,
-        name = dbFile.absolutePath,
-      ).setDriver(BundledSQLiteDriver())
+//    val dbFile = app.getDatabasePath(DB_FILENAME)
+//    return Room
+//      .databaseBuilder<AppDatabase>(
+//        context = app,
+//        name = dbFile.absolutePath,
+//      ).setDriver(BundledSQLiteDriver())
+//      .setQueryCoroutineContext(Dispatchers.IO)
+//      .build()
+
+
+    return Room.databaseBuilder(
+      app,
+      AppDatabase::class.java,
+      DB_FILENAME
+    )
+//      .setDriver(BundledSQLiteDriver())
       .setQueryCoroutineContext(Dispatchers.IO)
+      .createFromAsset(DB_FILENAME)
       .build()
 
   }
