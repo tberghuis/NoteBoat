eb1e48e13d413eab6834b609f7d9fc6d874c976b
Thomas Berghuis <thomas.berghuis@gmail.com>
2026-04-08 14:13:32 +0800

    .

--- androidApp/src/main/kotlin/xyz/tberghuis/noteboat/MainApplication.kt
+++ androidApp/src/main/kotlin/xyz/tberghuis/noteboat/MainApplication.kt
@@ -10,7 +10,10 @@ import android.util.Log
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.launch
+import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.compose.koinViewModel
+import xyz.tberghuis.noteboat.data.PreferencesRepository
 import xyz.tberghuis.noteboat.di.initKoin
 import xyz.tberghuis.noteboat.receiver.ScreenReceiver
 import xyz.tberghuis.noteboat.utils.logd
@@ -21,6 +24,7 @@ class MainApplication : Application() {
     addAction(Intent.ACTION_USER_PRESENT)
   }
   private val screenReceiver = ScreenReceiver()
+  private val userPrefs: PreferencesRepository by inject()
 
   override fun onCreate() {
     super.onCreate()
@@ -29,29 +33,27 @@ class MainApplication : Application() {
       androidContext(this@MainApplication)
     }
 
-
-
-
     channelLockScreen()
     registerScreenReceiver()
   }
 
   private fun registerScreenReceiver() {
-//    logd("registerScreenReceiver")
-//
-//    CoroutineScope(IO).launch {
-//      preferencesRepository.showShortcutLockScreenFlow.collect { showShortcut ->
-//        if (showShortcut) {
-//          registerReceiver(screenReceiver, screenReceiverIntentFilter)
-//        } else {
-//          try {
-//            unregisterReceiver(screenReceiver)
-//          } catch (e: IllegalArgumentException) {
-//            Log.e("MainApplication", "$e")
-//          }
-//        }
-//      }
-//    }
+    logd("registerScreenReceiver")
+
+    CoroutineScope(IO).launch {
+
+      userPrefs.showShortcutLockScreenFlow.collect { showShortcut ->
+        if (showShortcut) {
+          registerReceiver(screenReceiver, screenReceiverIntentFilter)
+        } else {
+          try {
+            unregisterReceiver(screenReceiver)
+          } catch (e: IllegalArgumentException) {
+            Log.e("MainApplication", "$e")
+          }
+        }
+      }
+    }
   }
 
   private fun channelLockScreen() {
