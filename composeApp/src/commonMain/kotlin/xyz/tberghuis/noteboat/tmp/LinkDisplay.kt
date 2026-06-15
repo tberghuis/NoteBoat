5eba3ffa30b801ded07f743f00fe79351e34b542
Thomas Berghuis <thomas.berghuis@gmail.com>
2026-06-15 12:22:47 +1000

    .

--- composeApp/src/commonMain/kotlin/xyz/tberghuis/noteboat/tmp/LinkDisplay.kt
+++ composeApp/src/commonMain/kotlin/xyz/tberghuis/noteboat/tmp/LinkDisplay.kt
@@ -1,23 +1,38 @@
 package xyz.tberghuis.noteboat.tmp
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.AnnotatedString
 import sh.calvin.autolinktext.rememberAutoLinkText
 
 @Composable
 fun LinkDisplay() {
 
-  Text(
-    AnnotatedString.rememberAutoLinkText(
-      """
+  Card(
+    modifier = Modifier.fillMaxWidth().clickable {
+      println("card clicked")
+
+    },
+  ) {
+    Text(
+      AnnotatedString.rememberAutoLinkText(
+        """
         |Visit https://www.google.com
         |Visit www.google.com
         |Email test@example.com
         |Call 6045557890
         |Call +1 (604) 555-7890
         |Call 604-555-7890
+        |hello.com
         """.trimMargin()
+      )
     )
-  )
+
+  }
+
+
 }
