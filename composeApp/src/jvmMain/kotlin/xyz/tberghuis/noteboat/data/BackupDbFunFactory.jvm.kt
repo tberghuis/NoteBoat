85bc70aeea912d23aa9bfa9c5713b463fa69e3fe
Thomas Berghuis <thomas.berghuis@gmail.com>
2026-06-27 12:35:04 +1000

    .

--- composeApp/src/jvmMain/kotlin/xyz/tberghuis/noteboat/data/BackupDbFunFactory.jvm.kt
+++ composeApp/src/jvmMain/kotlin/xyz/tberghuis/noteboat/data/BackupDbFunFactory.jvm.kt
@@ -1,7 +1,31 @@
 package xyz.tberghuis.noteboat.data
 
-actual class BackupDbFunFactory {
+import androidx.room3.useWriterConnection
+import java.io.File
+import xyz.tberghuis.noteboat.DB_FILENAME
+
+actual class BackupDbFunFactory(
+  private val db: AppDatabase,
+) {
   actual fun create(): suspend (backupFileUri: String) -> Unit {
-    TODO("Not yet implemented")
+    return { backupFileUri ->
+      // checkpoint
+      db.useWriterConnection { transactor ->
+        transactor.usePrepared("pragma wal_checkpoint(full)") { stmt ->
+          if (stmt.step()) {
+            val busy = stmt.getLong(0)
+            val log = stmt.getLong(1)
+            val checkpointed = stmt.getLong(2)
+            println("backupDb busy $busy log $log checkpointed $checkpointed")
+          }
+        }
+      }
+      val dbFile = File(System.getProperty("java.io.tmpdir"), DB_FILENAME)
+      dbFile.copyTo(
+        target = File(backupFileUri),
+        overwrite = true,
+      )
+
+    }
   }
 }
