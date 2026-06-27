50bdc586447f290b60078b91ddc3098e75833f46
Thomas Berghuis <thomas.berghuis@gmail.com>
2026-06-27 12:40:10 +1000

    .

--- composeApp/src/jvmMain/kotlin/xyz/tberghuis/noteboat/data/ImportDbFunFactory.jvm.kt
+++ composeApp/src/jvmMain/kotlin/xyz/tberghuis/noteboat/data/ImportDbFunFactory.jvm.kt
@@ -1,7 +1,28 @@
 package xyz.tberghuis.noteboat.data
 
-actual class ImportDbFunFactory {
+import androidx.room3.Room
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
+import java.io.File
+import kotlinx.coroutines.flow.first
+
+actual class ImportDbFunFactory(
+  private val noteDao: NoteDao,
+) {
   actual fun create(): suspend (importDbUri: String) -> Unit {
-    TODO("Not yet implemented")
+    return { importDbUri ->
+      val roomImport = Room.databaseBuilder<AppDatabase>(
+        name = File(importDbUri).absolutePath
+      ).setDriver(BundledSQLiteDriver())
+        .build()
+      // read all notes
+      // if invalid file, room will log error and give me empty notes list
+      val importNotesList = roomImport.noteDao().getAll().first().map {
+        it.copy(noteId = 0)
+      }
+      // write to appDatabase
+      noteDao.insertAll(importNotesList)
+      // close import db
+      roomImport.close()
+    }
   }
 }
