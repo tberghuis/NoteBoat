db14fd211c00e7857efa45f276b81ebc0a4e51a0
Thomas Berghuis <thomas.berghuis@gmail.com>
2026-06-27 12:17:07 +1000

    .

--- composeApp/src/jvmMain/kotlin/xyz/tberghuis/noteboat/composable/BackupDatabaseButton.jvm.kt
+++ composeApp/src/jvmMain/kotlin/xyz/tberghuis/noteboat/composable/BackupDatabaseButton.jvm.kt
@@ -1,7 +1,9 @@
 package xyz.tberghuis.noteboat.composable
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 actual fun BackupDatabaseButton() {
+  Text("hello backup button")
 }
--- composeApp/src/jvmMain/kotlin/xyz/tberghuis/noteboat/screen/SettingsScreen.jvm.kt
+++ composeApp/src/jvmMain/kotlin/xyz/tberghuis/noteboat/screen/SettingsScreen.jvm.kt
@@ -1,8 +1,43 @@
 package xyz.tberghuis.noteboat.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import xyz.tberghuis.noteboat.composable.BackupDatabaseButton
 
 @Composable
 actual fun SettingsContent(padding: PaddingValues) {
+
+
+  Column(
+    modifier = Modifier
+      .padding(padding)
+      .verticalScroll(rememberScrollState())
+      .fillMaxSize()
+  ) {
+
+    SettingsScreenRow(
+      horizontalArrangement = Arrangement.Center,
+    ) {
+      BackupDatabaseButton()
+    }
+    SettingsScreenRow(
+      horizontalArrangement = Arrangement.Center,
+    ) {
+//      DeleteAllNotesButton()
+    }
+    SettingsScreenRow(
+      horizontalArrangement = Arrangement.Center,
+    ) {
+//      ImportFromBackupButton()
+    }
+  }
+
+
 }
