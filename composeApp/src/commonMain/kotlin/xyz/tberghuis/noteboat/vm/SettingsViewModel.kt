e02e8f4d0beb3ad22e51b180f668400d9a3dc425
Thomas Berghuis <thomas.berghuis@gmail.com>
2026-06-27 11:20:27 +1000

    .

--- composeApp/src/commonMain/kotlin/xyz/tberghuis/noteboat/vm/SettingsViewModel.kt
+++ composeApp/src/commonMain/kotlin/xyz/tberghuis/noteboat/vm/SettingsViewModel.kt
@@ -1,27 +1,15 @@
 package xyz.tberghuis.noteboat.vm
 
-import android.app.Application
-import android.net.Uri
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.core.net.toUri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.room3.Room
-import androidx.room3.useWriterConnection
-import java.io.File
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
-import xyz.tberghuis.noteboat.DB_FILENAME
-import xyz.tberghuis.noteboat.IMPORT_DB_FILENAME
-import xyz.tberghuis.noteboat.data.AppDatabase
-import xyz.tberghuis.noteboat.data.BackupDbFunFactory
 import xyz.tberghuis.noteboat.data.NoteDao
 import xyz.tberghuis.noteboat.data.PreferencesRepository
-import xyz.tberghuis.noteboat.utils.logd
 
 class SettingsViewModel(
   private val preferencesRepository: PreferencesRepository,
